c1e5df34af57bbcdf3ff7a1ccad1dc5643717314
Crowdbotics <team@crowdbotics.com>
2021-08-07 12:23:59 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "rapid_wave_29603Identifier",
+  url: "https://rapid_wave_29603Identifier.botics.co",
+  api: "https://rapid_wave_29603Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
